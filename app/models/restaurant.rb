8418f92591ff1aa6167b2747b68c5f7f34c79707
Gianluca Abad <gianlucaabad@gmail.com>
2018-05-23 12:07:33 +0900

    added validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
-
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"]}
 end
